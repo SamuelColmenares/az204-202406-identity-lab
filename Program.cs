0995a9ab9fd1585c308ba1cf197b1b65ba180386
Samuel Colmenares <samuelcolmenares@MacBook-Pro-de-Samuel.local>
2024-06-19 21:42:08 -0500

    codigo inicial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+Console.WriteLine("Inicia Aplicacion..");
+
+string clientId = "<<Application ID>>";
+string tenantId = "<<Tenant ID>>";
+
+var app = PublicClientApplicationBuilder
+    .Create(clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "user.read" };
+
+AuthenticationResult authResult = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+Console.WriteLine($"Token recibido:\t{authResult.AccessToken}");
